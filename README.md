9092cc1831e80b790c7a705d360d266233febfea
dvogt23 <divogt@live.de>
2017-12-15 00:08:43 +0100

    add file commands + README

--- README.md
+++ README.md
@@ -2,24 +2,37 @@
 A pass extension that apply some file functions
 
 ## Description
-
+*pass-file* extends the pass with a file command thats provide a command to add files to passwordstore and get files back from passwordstore to the current location.
 
 ## Usage
+```
+pass file 1.0 - A pass extension that apply to add a file to passwordstore
 
+Usage:
+        pass file [-a <file> <pass item>] [-g <pass-name>] [-V] [-h]
+            Provide a command to add files to passwordstore and
+            get files back from passwordstore to the current location.
 
-## Examples
+        Options:
+            a, add          Add file to passwordstore
+            g, get          Get file from passwordstore
+            -v, --version   Show version information.
+            -h, --help      Print this help message and exit.
 
-
+    More information may be found in the pass-file(1) man page.
+```
 
 ## Installation
+If you do not want to install this extension as system extension, you need to enable user extension with PASSWORD_STORE_ENABLE_EXTENSIONS=true pass. You can create an alias in .bashrc: alias pass='PASSWORD_STORE_ENABLE_EXTENSIONS=true pass'
 
+**Requirements**
+* pass 1.7.0 or greater.
 
 ## Contribution
 Feedback, contributors, pull requests are welcome.
 
-
 ## License
-
+```
     Copyright (C) 2017  Alexandre PUJOL
 
     This program is free software: you can redistribute it and/or modify
@@ -34,3 +47,4 @@ Feedback, contributors, pull requests are welcome.
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+```
--- file.bash
+++ file.bash
@@ -14,6 +14,95 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+#	gpg -o .passwordstore/test/test.txt.gpg -e -r divogt@live.de test.txt
+#	gpg --output foo.txt --decrypt foo.txt.gpg
 
 readonly VERSION="1.0"
+
+cmd_file_version() {
+	cat <<-_EOF
+	$PROGRAM $COMMAND $VERSION - A pass extension that apply to add a file to passwordstore
+	_EOF
+}
+
+cmd_file_usage() {
+	cmd_file_version
+	echo
+	cat <<-_EOF
+	Usage:
+        $PROGRAM $COMMAND [-a <file> <pass item>] [-g <pass-name>] [-V] [-h]
+            Provide a command to add files to passwordstore and
+            get files back from passwordstore to the current location.
+
+    	Options:
+            a, add           Add file to passwordstore
+            g, get           Get file from passwordstore
+            -v, --version    Show version information.
+            -h, --help       Print this help message and exit.
+
+	More information may be found in the pass-file(1) man page.
+	_EOF
+}
+
+# Insert data to the password store
+# $1: Path in the password store
+# $2: Filename
+_insert() {
+	local path="${1%/}" FILENAME="$2" passfile="$PREFIX/$path/$FILENAME.gpg"
+
+	# echo "Path: $path"
+	# echo "Filename: $FILENAME"
+	# echo "Passfile: $passfile"
+	mapfile < $FILENAME
+
+	set_git "$passfile"
+	mkdir -p -v "$PREFIX/$path"
+	set_gpg_recipients "$path/$FILENAME"
+	$GPG -e "${GPG_RECIPIENT_ARGS[@]}" -o "$passfile" "${GPG_OPTS[@]}" <<<"${MAPFILE[@]}" || \
+	 	die "Error: File could not be stored."
+	git_add_file "$passfile" "Insert file $path to store."
+}
+
+cmd_file_add() {
+	# Sanity checks
+	[[ -z "${*}" ]] && die "Usage: $PROGRAM $COMMAND [-a <file> <pass item>] [-g <pass-name>] [-V] [-h]"
+	[[ ! -f $1 ]] && die "Error: File $1 must be exist."
+	[[ -z "$2" ]] && die "Usage: $PROGRAM $COMMAND [-a <file> <pass item>] [-g <pass-name>] [-V] [-h]"
+
+	local path="${2%/}"
+	local passfile="$PREFIX/$path.gpg"
+	local FILENAME="$1"
+	local PASSPATH="$2"
+
+	printf "\e[1m\e[37mAdd file $FILENAME to \e[4m%s\e[0m\n" "$path"
+
+	_insert $path $FILENAME
+
+	exit 0;
+}
+
+# Create file from pass
+cmd_file_get() {
+	# Sanity checks
+	[[ -z "${*}" ]] && die "Usage: $PROGRAM $COMMAND [-a <file> <pass item>] [-g <pass-name>] [-V] [-h]"
+	[[ ! -f $PREFIX/$1.gpg ]] && die "Error: File $1 must be exist."
+
+	local path="$1"
+	local passfile="$PREFIX/$path.gpg"
+	local filename="$(basename $path)"
+	[[ -f $passfile ]] && { $GPG -d "${GPG_OPTS[@]}" "$passfile" > $filename || exit $?; }
+	exit 0;
+}
+
+# Getopt options
+while true; do case $1 in
+	a|add) shift; cmd_file_add "$@" ;;
+	g|get) shift; cmd_file_get "$@" ;;
+	-h|--help) shift; cmd_file_usage; exit 0 ;;
+	-v|--version) shift; cmd_file_version; exit 0 ;;
+	--) shift; break ;;
+esac done
+
+# General Start
+[[ $err -ne 0 ]] && cmd_file_usage && exit 1
+cmd_file "$@"
